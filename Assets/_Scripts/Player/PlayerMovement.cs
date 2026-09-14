7a51dc7c20e99079ec540c5887ed44745507bd42
HirumiIsna <hilmiisnaeni@student.ub.ac.id>
2026-09-14 17:57:18 +0700

    feat: character animation

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -6,12 +6,19 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float walkSpeed = 8f;
     [SerializeField] private float runSpeed = 16f;
     private Vector2 moveInput;
+    private bool isMoving;
+    private bool isSprinting;
+
+    #region [Components]
     private Rigidbody2D rigidbody;
+    private Animator anim;
+    #endregion
 
     #region [Unity Lifecycle Method]
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     private void Start()
@@ -22,6 +29,8 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         HandleInput();
+        HandleAnimation();
+        HandleFlip();
     }
 
     private void FixedUpdate()
@@ -40,12 +49,34 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleMoving()
     {
-        float speed = InputManager.Instance.GetPlayerSprinting() ? runSpeed : walkSpeed;
+        isSprinting = InputManager.Instance.GetPlayerSprinting();
+        float speed = isSprinting ? runSpeed : walkSpeed;
         rigidbody.linearVelocity = new Vector2(moveInput.x * speed, rigidbody.linearVelocity.y);
+    }
+
+    private void HandleAnimation()
+    {
+        isMoving = Mathf.Abs(moveInput.x) > 0.01f;
+
+        bool isWalking = isMoving && !isSprinting; 
+        bool isRunning = isMoving && isSprinting; 
+
+        anim.SetBool("isWalking", isWalking); 
+        anim.SetBool("isRunning", isRunning);
     }
     #endregion
 
     #region [Helper Method]
-    private void HandleSpriteFlip() { }
+    private void HandleFlip() 
+    {
+        if (Mathf.Abs(moveInput.x) < 0.01f) return; 
+        
+        Vector3 localScale = transform.localScale; 
+        
+        if (moveInput.x > 0) localScale.x = Mathf.Abs(localScale.x); 
+        else if (moveInput.x < 0) localScale.x = -Mathf.Abs(localScale.x);
+        
+        transform.localScale = localScale;
+    }
     #endregion
 }
